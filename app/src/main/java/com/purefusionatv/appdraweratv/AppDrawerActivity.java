3c452f3755abfe0b1eb16fbc426d0c71a7f6748d
Eliminater74 <eliminater74@gmail.com>
2020-02-26 21:05:25 -0500

    [AppDrawerATV] Optimize Imports: Uneeded imports:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- app/src/main/java/com/purefusionatv/appdraweratv/AppDrawerActivity.java
+++ app/src/main/java/com/purefusionatv/appdraweratv/AppDrawerActivity.java
@@ -18,14 +18,11 @@ package com.purefusionatv.appdraweratv;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
